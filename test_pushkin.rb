af7f599ed4d1f19cd51edcf4c75928c79f8ee832
Roman Vakulchik <roman.vakulchik@gmail.com>
2015-03-14 23:36:04 +0300

    restore assets, helpers, mailers, models, views

--- test_pushkin.rb
+++ test_pushkin.rb
@@ -1,12 +1,8 @@
 require 'net/http'
 require 'pry'
 
-postData = Net::HTTP.post_form(URI.parse('http://localhost:3000/registration'), {
-  "question" => "Отчизны внемлем призыванье",
-  "token" => "dfhjasdkf9527398457sdjfhk"
-  #"id"       => 6595, # TASK_ID
-  #"level"    => 1,
-})
+postData = Net::HTTP.post_form(URI.parse('http://localhost:3000/registration'),
+  {"question"=>"К Стамбулу грозно притекли", "id"=>55270, "level"=>1})
 
 binding.pry
 
