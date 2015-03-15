bd92d76d6e9cbde002030a7d01bb541e165e9ce0
Roman Vakulchik <roman.vakulchik@gmail.com>
2015-03-15 11:02:34 +0300

    add rails12-factor

--- test_pushkin.rb
+++ test_pushkin.rb
@@ -1,7 +1,7 @@
 require 'net/http'
 require 'pry'
 
-postData = Net::HTTP.post_form(URI.parse('http://localhost:3000/quiz'),
+postData = Net::HTTP.post_form(URI.parse('http://chappie.herokuapp.com/quiz'),
   {"question"=>"К Стамбулу грозно притекли", "id"=>55270, "level"=>1})
 
 binding.pry
